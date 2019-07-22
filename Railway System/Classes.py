9b873ef517be10162272c417719c2dc2fd4f36ac
MezoZoli <49882408+MezoZoli@users.noreply.github.com>
2019-07-22 21:06:52 +0200

    Add files via upload

---
+++ Railway System/Classes.py
@@ -0,0 +1,33 @@
+import random
+import time
+
+
+class bcolors:
+    HEADER = "\033[95m"
+    OKBLUE = "\033[94m"
+    OKGREEN = "\033[92m"
+    WARNING = "\033[93m"
+    FAIL = '\033[91m'
+    ENDC = '\033[0m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+
+
+Cargo_Train = 0
+Passenger_Train = 1
+Intercity = 2
+Waiting = 3
+
+while True:
+    importance = random.randrange(Waiting)
+    if importance is 0:
+        time.sleep(1)
+        print("Here comes a" + bcolors.FAIL + bcolors.BOLD + " Cargo Train!" + bcolors.ENDC)
+    if importance is 1:
+        time.sleep(1)
+        print("Here comes a" + bcolors.OKBLUE + bcolors.BOLD + " Passenger Train!" + bcolors.ENDC)
+    if importance is 2:
+        time.sleep(1)
+        print("Here comes an" + bcolors.HEADER + bcolors.BOLD + " Intercity!" + bcolors.ENDC)
+
+# majd amikor egy vonat végigmegy, akkor jön majd új vonat
